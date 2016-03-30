5bef5ce47e434d99e1359cb8093322ffba60aabb
李常龙 <1304027928@qq.com>
2016-03-30 16:08:33 +0800

    iso

--- src/ios/pickCrop.m
+++ src/ios/pickCrop.m
@@ -43,19 +43,19 @@
 {
     
     
-    // NSLog(@"Cordova iOS Cache.clear() called.");
-    // pickerController *pc = [[pickerController alloc]init];
-    //获取方式1：通过相册（呈现全部相册），UIImagePickerControllerSourceTypePhotoLibrary
-    //获取方式2，通过相机，UIImagePickerControllerSourceTypeCamera
-    //获取方方式3，通过相册（呈现全部图片），UIImagePickerControllerSourceTypeSavedPhotosAlbum
-    // pc.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;//方式1
-    // //允许编辑，即放大裁剪
-    // pc.allowsEditing = YES;
-    // //自代理
-    // pc.delegate = self;
-    // self.callbackId = command.callbackId;
-    // NSLog(@"-------------callbackId-------------------------");
-    // NSLog(self.callbackId);
+    NSLog(@"Cordova iOS Cache.clear() called.");
+    pickerController *pc = [[pickerController alloc]init];
+    // 获取方式1：通过相册（呈现全部相册），UIImagePickerControllerSourceTypePhotoLibrary
+    // 获取方式2，通过相机，UIImagePickerControllerSourceTypeCamera
+    // 获取方方式3，通过相册（呈现全部图片），UIImagePickerControllerSourceTypeSavedPhotosAlbum
+    pc.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;//方式1
+    //允许编辑，即放大裁剪
+    pc.allowsEditing = YES;
+    //自代理
+    pc.delegate = self;
+    self.callbackId = command.callbackId;
+    NSLog(@"-------------callbackId-------------------------");
+    NSLog(self.callbackId);
 //
 //    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
 //                                                      messageAsString:@"FFF"];
@@ -66,82 +66,9 @@
 //    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
     
     //    [self.viewController addChildViewController:childView];
-    // [self.viewController presentViewController:pc animated:YES completion:NULL];
+    [self.viewController presentViewController:pc animated:YES completion:NULL];
     // 
-     self.hasPendingOperation = YES;
-    
-    __weak CDVCamera* weakSelf = self;
-
-    [self.commandDelegate runInBackground:^{
-        
-        CDVPictureOptions* pictureOptions = [CDVPictureOptions createFromTakePictureArguments:command];
-        pictureOptions.popoverSupported = [weakSelf popoverSupported];
-        pictureOptions.usesGeolocation = [weakSelf usesGeolocation];
-        pictureOptions.cropToSize = NO;
-        
-        BOOL hasCamera = [UIImagePickerController isSourceTypeAvailable:pictureOptions.sourceType];
-        if (!hasCamera) {
-            NSLog(@"Camera.getPicture: source type %lu not available.", (unsigned long)pictureOptions.sourceType);
-            CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"No camera available"];
-            [weakSelf.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-            return;
-        }
-
-        // Validate the app has permission to access the camera
-        if (pictureOptions.sourceType == UIImagePickerControllerSourceTypeCamera && [AVCaptureDevice respondsToSelector:@selector(authorizationStatusForMediaType:)]) {
-            AVAuthorizationStatus authStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
-            if (authStatus == AVAuthorizationStatusDenied ||
-                authStatus == AVAuthorizationStatusRestricted) {
-                // If iOS 8+, offer a link to the Settings app
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wtautological-pointer-compare"
-                NSString* settingsButton = (&UIApplicationOpenSettingsURLString != NULL)
-                    ? NSLocalizedString(@"Settings", nil)
-                    : nil;
-#pragma clang diagnostic pop
-
-                // Denied; show an alert
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    [[[UIAlertView alloc] initWithTitle:[[NSBundle mainBundle]
-                                                         objectForInfoDictionaryKey:@"CFBundleDisplayName"]
-                                                message:NSLocalizedString(@"Access to the camera has been prohibited; please enable it in the Settings app to continue.", nil)
-                                               delegate:weakSelf
-                                      cancelButtonTitle:NSLocalizedString(@"OK", nil)
-                                      otherButtonTitles:settingsButton, nil] show];
-                });
-            }
-        }
-
-        CDVCameraPicker* cameraPicker = [CDVCameraPicker createFromPictureOptions:pictureOptions];
-        weakSelf.pickerController = cameraPicker;
-        
-        cameraPicker.delegate = weakSelf;
-        cameraPicker.callbackId = command.callbackId;
-        // we need to capture this state for memory warnings that dealloc this object
-        cameraPicker.webView = weakSelf.webView;
-        
-        // Perform UI operations on the main thread
-        dispatch_async(dispatch_get_main_queue(), ^{
-            // If a popover is already open, close it; we only want one at a time.
-            if (([[weakSelf pickerController] pickerPopoverController] != nil) && [[[weakSelf pickerController] pickerPopoverController] isPopoverVisible]) {
-                [[[weakSelf pickerController] pickerPopoverController] dismissPopoverAnimated:YES];
-                [[[weakSelf pickerController] pickerPopoverController] setDelegate:nil];
-                [[weakSelf pickerController] setPickerPopoverController:nil];
-            }
-
-            if ([weakSelf popoverSupported] && (pictureOptions.sourceType != UIImagePickerControllerSourceTypeCamera)) {
-                if (cameraPicker.pickerPopoverController == nil) {
-                    cameraPicker.pickerPopoverController = [[NSClassFromString(@"UIPopoverController") alloc] initWithContentViewController:cameraPicker];
-                }
-                [weakSelf displayPopover:pictureOptions.popoverOptions];
-                weakSelf.hasPendingOperation = NO;
-            } else {
-                [weakSelf.viewController presentViewController:cameraPicker animated:YES completion:^{
-                    weakSelf.hasPendingOperation = NO;
-                }];
-            }
-        });
-    }];
+     
     
 }
 //PickerImage完成后的代理方法
